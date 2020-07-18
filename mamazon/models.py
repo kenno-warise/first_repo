ebe8e11d8a7ee027fc76225a9aa7ff15c531a8c1
Kenno-Warise <wariken0523@gmail.com>
2020-07-18 13:30:59 +0900

    lesson03

--- mamazon/models.py
+++ mamazon/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    """
+    商品モデル
+    name: 商品名
+    price: 商品の値段
+    description: 商品の説明
+    thumbnail: 商品の写真
+    """
+    name = models.CharField(max_length=150)
+    price = models.IntegerField()
+    description = models.TextField()
+    thumbnail = models.ImageField(upload_to='thumbnails/') # 写真の保存先を指定
